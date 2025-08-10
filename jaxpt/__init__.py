455ca86f786497f2b7765ebd2b6af7d886942b49
vschac <vincent.schacknies@icloud.com>
2025-08-10 17:49:46 -0400

    updated version number

--- jaxpt/__init__.py
+++ jaxpt/__init__.py
@@ -4,7 +4,7 @@ JAX-PT: JAX-accelerated FAST-PT for computing perturbation theory power spectra
 
 from .JAXPT import JAXPT
 
-__version__ = "1.0.1"
+__version__ = "1.0.0"
 __author__ = "Vincent Schacknies"
 __email__ = "vincent.schacknies@icloud.com"
 
